d9a6d46fcda24b4f35b9a689c0255180e22fca1a
Junior Dugue <jrdugue@gmail.com>
2019-07-09 14:10:42 -0400

    created new class component called scorekeeper
    created new class component called scorekeeper

---
+++ src/components/ScoreKeeper/ScoreKeeper.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class ScoreKeeper extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>ScoreKeeper Component</h1>
+      </div>
+    );
+  }
+}
